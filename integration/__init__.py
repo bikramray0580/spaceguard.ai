3c297764a41feb53c5cc622b30a4e5b688b354b4
Bikram Ray <raybikram2005@gmail.com>
2026-09-11 15:37:27 +0530

    Export database-backed integration pipeline

--- integration/__init__.py
+++ integration/__init__.py
@@ -1,7 +1,13 @@
-"""Orchestration for the SpaceGuard A -> B -> C processing pipeline."""
+"""Orchestration for the SpaceGuard orbital processing pipeline."""
 
 from .models import LoadedOrbitalData, PipelineRun, SatelliteTLE
-from .pipeline import generate_shared_timestamps, load_orbital_data, run_pipeline
+from .pipeline import (
+    generate_shared_timestamps,
+    load_orbital_data,
+    load_orbital_data_from_database,
+    run_pipeline,
+    run_pipeline_from_database,
+)
 
 __all__ = [
     "LoadedOrbitalData",
@@ -9,5 +15,7 @@ __all__ = [
     "SatelliteTLE",
     "generate_shared_timestamps",
     "load_orbital_data",
+    "load_orbital_data_from_database",
     "run_pipeline",
+    "run_pipeline_from_database",
 ]
